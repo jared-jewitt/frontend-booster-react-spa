5f0f7377106156f3b3bea901488af078a274a534
Jared Jewitt <jaredjewitt@gmail.com>
2020-03-08 12:59:08 -0700

    Update Docker files and README

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,9 @@
-const path = require('path');
-
 module.exports = {
   parser: 'babel-eslint',
   env: {
     node: true,
     browser: true,
     jest: true,
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-    'import/resolver': {
-      alias: {
-        map: [['@', path.join(__dirname, 'src')]],
-      },
-    },
   },
   plugins: ['jest', 'react'],
   extends: ['eslint:recommended', 'plugin:jest/recommended', 'plugin:react/recommended'],
