0b1026052dc0693739b9f6d03f3a7036f96db82a
Jared Jewitt <jaredjewitt@gmail.com>
2020-04-29 11:02:53 -0700

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   testURL: 'http://localhost/',
   testEnvironment: 'jsdom',
   setupFiles: ['<rootDir>/jest.setup.js'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': 'jest-transform-stub',
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
